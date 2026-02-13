eb975f53aa2ff01fa2ca732a4d5d25063b24c7e8
Dedes <federico.lepore73@gmail.com>
2026-02-13 13:27:21 +0100

    gestione overflow

--- src/component/NavbarComponent.jsx
+++ src/component/NavbarComponent.jsx
@@ -6,9 +6,9 @@ import { Link } from "react-router-dom";
 
 const NavbarComponent = function (props) {
     return (
-        <Container fluid className="d-flex flex-column bg-body-tertiary">
-            <h1 className="text-center text-md-start p-3 pb-0 mb-0 ms-md-5">{props.title}</h1>
-            <div className='d-flex justify-content-center align-items-center w-100 gap-5'>
+        <Container fluid className="d-flex flex-column bg-body-tertiary px-0">
+            <h1 className="text-center text-md-start p-3 pb-0 mb-0 ">{props.title}</h1>
+            <div className='d-flex justify-content-center align-items-center w-100 gap-3 gap-md-5 px-2'>
                 {props.link.map((link, index) => {
                     return <Link key={index} className="text-decoration-none p-3" to={link.path}>{link.name}</Link>
                 })}
